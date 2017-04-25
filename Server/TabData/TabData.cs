d5c577b9c53588fa433a63f0416d2cb033b239f2
Patryk Strach <Strachu@users.noreply.github.com>
2017-04-25 19:48:34 +0200

    Add dedicated Id column as key instead of index which changes often.

--- Server/TabData/TabData.cs
+++ Server/TabData/TabData.cs
@@ -1,5 +1,6 @@
 public class TabData
 {
+	public int Id { get; set; }
 	public int Index { get; set; }
 	public string Url { get; set; }
 }
--- Server/TabData/TabDataMapping.cs
+++ Server/TabData/TabDataMapping.cs
@@ -6,11 +6,11 @@ public class TabDataMapping : EntityTypeConfiguration<TabData>
 {
 	public override void Map(EntityTypeBuilder<TabData> builder)
 	{
-		builder.ToTable("Tabs");
+		builder.ToTable("OpenTabs");
 
-		builder.HasKey(x => x.Index);
-		builder.Property(x => x.Index).ValueGeneratedNever();
-		
+		builder.HasKey(x => x.Id);
+
+		builder.Property(x => x.Index);
 		builder.Property(x => x.Url)
 			.HasMaxLength(8192) // http://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
 			.IsRequired();
