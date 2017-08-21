5ee598a99b42cf9f599d924c539e171105ce3854
Patryk Strach <Strachu@users.noreply.github.com>
2017-08-21 21:59:51 +0200

    FIX: Race condition during reading all tabs' url on android.

--- Client/Firefox/Android/Overrides.js
+++ Client/Firefox/Android/Overrides.js
@@ -4,21 +4,38 @@ tabManager.getAllTabsWithUrls = function() {
 
         // TODO This is needed on android because tab.url is "about:blank" on all
         // tabs not yet activated. But activating every tab can be bad for performance - needs profiling.
-        // But still some tabs are not included. Need to wait some time??
         browser.tabs.query({}).then(function(tabs) {
 
             var promises = [];
             for (var i = 0; i < tabs.length; i++) {
-                promises.push(browser.tabs.update(tabs[i].id, { active: true }));
+                promises.push(ensureTabUrlReady(tabs[i].id));
             }
 
             Promise.all(promises).then(function() {
-
                 browser.tabs.query({}).then(resolve);
-
             });
         });
-
     });
-
 };
+
+function ensureTabUrlReady(tabId) {
+    return browser.tabs.update(tabId, { active: true })
+        .then(function(tab) {
+
+            if (tab.url && tab.url !== "about:blank")
+                return;
+
+            // The url is not available yet when update returns...
+            return new Promise(function(resolve) {
+                var currentTabUrlUpdatedClosure = function(tabId, changeInfo) {
+
+                    if (changeInfo.url) {
+                        resolve();
+                        browser.tabs.onUpdated.removeListener(currentTabUrlUpdatedClosure);
+                    }
+                };
+
+                browser.tabs.onUpdated.addListener(currentTabUrlUpdatedClosure);
+            });
+        });
+}
