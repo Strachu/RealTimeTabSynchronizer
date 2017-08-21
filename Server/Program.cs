2950336eda29aa6da3bce0de05202d6bebee48a4
Patryk Strach <Strachu@users.noreply.github.com>
2017-08-21 22:02:09 +0200

    Changed spaces to tabs in Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,36 +11,36 @@ using RealTimeTabSynchronizer.Server.EntityFramework;
 
 namespace RealTimeTabSynchronizer.Server
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var hostingConfiguration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var hostingConfiguration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.Build();
 
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseConfiguration(hostingConfiguration)
-                .UseStartup<Startup>()
-                .Build();
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseConfiguration(hostingConfiguration)
+				.UseStartup<Startup>()
+				.Build();
 
-            RecreateDatabase(host.Services.GetService(typeof(DbContextFactory)) as DbContextFactory);
+			RecreateDatabase(host.Services.GetService(typeof(DbContextFactory)) as DbContextFactory);
 
-            host.Run();
-        }
+			host.Run();
+		}
 
-        // TODO Change to migrations
-        private static void RecreateDatabase(DbContextFactory contextFactory)
-        {
-                using (var uow = contextFactory.Create())
-                {
-                    uow.Database.EnsureDeleted();
-                    uow.Database.EnsureCreated();
-                    uow.SaveChanges();
-                }
-        }
-    }
+		// TODO Change to migrations
+		private static void RecreateDatabase(DbContextFactory contextFactory)
+		{
+			using (var uow = contextFactory.Create())
+			{
+				uow.Database.EnsureDeleted();
+				uow.Database.EnsureCreated();
+				uow.SaveChanges();
+			}
+		}
+	}
 }
